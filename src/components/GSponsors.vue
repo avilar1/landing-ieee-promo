76ce6885b4acce59cd59d5b8ff2b4d28bb1bef64
YagoA <yagoavila1@gmail.com>
2025-10-31 19:40:44 -0300

    mudado nome da classe de sponsors para evitar bloqueio de adblock

--- src/components/GSponsors.vue
+++ src/components/GSponsors.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="sponsors-geral">
-        <div class="sponsors-container">
-            <h2 class="sponsors-h2">Organization & Sponsorship</h2>
-            <div class="sponsors-box">
+    <div class="supporters-geral">
+        <div class="supporters-container">
+            <h2 class="supporters-h2">Organization & Sponsorship</h2>
+            <div class="supporters-box">
                 <h4>Organized by:</h4>
-                <div class="sponsors-organized">
+                <div class="supporters-logos">
                     <img src="../../public/images/S11.png" alt="sponsor 1">
                     <img src="../../public/images/S12.png" alt="sponsor 2">
                 </div>
             </div>
-            <div class="sponsors-box">
+            <div class="supporters-box">
                 <h4>Supported by:</h4>
-                <div class="sponsors-organized">
+                <div class="supporters-logos">
                     <img src="../../public/images/S11.png" alt="sponsor 1">
                     <img src="../../public/images/S22.png" alt="sponsor 2">
                     <img src="../../public/images/S12.png" alt="sponsor 3">
                     <img src="../../public/images/S11.png" alt="sponsor 4">
                 </div>
             </div>
-            <div class="sponsors-box">
+            <div class="supporters-box">
                 <h4>Other partnerships:</h4>
-                <div class="sponsors-organized">
+                <div class="supporters-logos">
                     <img src="../../public/images/S11.png" alt="sponsor 1">
                     <img src="../../public/images/S12.png" alt="sponsor 2">
                     <img src="../../public/images/S22.png" alt="sponsor 3">
                     <img src="../../public/images/S12.png" alt="sponsor 4">
-
                 </div>
             </div>
         </div>
@@ -33,17 +32,15 @@
 </template>
 
 <style scoped>
-.sponsors-geral {
-
+.supporters-geral {
     background-color: var(--neutral-color-light);
     display: flex;
     align-items: center;
     justify-content: center;
-
     padding: 96px 0;
 }
 
-.sponsors-h2 {
+.supporters-h2 {
     font-size: 32px;
     font-weight: 700;
     text-align: center;
@@ -53,7 +50,7 @@
     margin-top: 24px;
 }
 
-.sponsors-container {
+.supporters-container {
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -62,20 +59,20 @@
     min-width: 1120px;
 }
 
-.sponsors-box {
+.supporters-box {
     display: flex;
     flex-direction: column;
     align-items: start;
     margin-bottom: 2rem;
 }
 
-.sponsors-box h4 {
+.supporters-box h4 {
     font-size: 24px;
     font-weight: 700;
     margin-bottom: 1rem;
 }
 
-.sponsors-organized {
+.supporters-logos {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -83,7 +80,7 @@
     gap: 2rem;
 }
 
-.sponsors-organized img {
+.supporters-logos img {
     width: 200px;
     height: auto;
 }
